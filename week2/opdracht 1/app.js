c0bd28d10cefa72f164c1e2d4d48bd56e48fad00
pascal-vegt <9016712@student.zadkine.nl>
2024-02-14 11:12:19 +0100

    opdracht

---
+++ week2/opdracht 1/app.js
@@ -0,0 +1,13 @@
+let animals = ["Kikker", "Koe", "Paard"];
+
+//for (let i = 0; i < animals.length; i++) {
+//  document.write(animals[i] + "<br>");
+//}
+
+//for (let i = 1; i <= 100; i++) {
+//  document.write(i + "<br>");
+//}
+
+//for (let i = 100; i >= 0; i--) {
+//  document.write(i + "<br>");
+//}
