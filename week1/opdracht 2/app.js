b063d5b69532e91b732717c7d3767d764ab6be80
pascal-vegt <9016712@student.zadkine.nl>
2024-02-14 11:31:33 +0100

    week 1 opdracht 2

---
+++ week1/opdracht 2/app.js
@@ -0,0 +1,21 @@
+let output = document.getElementById("paragraph");
+let input = document.getElementById("inputform");
+let button = document.getElementById("submitbutton");
+
+let shop = [];
+
+button.addEventListener("click", function add_product() {
+  if (input.value != "") {
+    shop.push(" " + input.value);
+    input.value = "";
+  } else {
+    alert("Wat je hebt ingevult is niet geldig");
+  }
+  output.innerText = shop + " ";
+});
+
+//Haal de waarde van je input op (de waarde is wat je typt in je input),
+//dit sla je op in een variabel genaamd "product".
+
+//Voeg nu de variabele product toe aan je array
+//(in de vorige opdracht heb je geleerd hoe je iets toevoegt in je array)
