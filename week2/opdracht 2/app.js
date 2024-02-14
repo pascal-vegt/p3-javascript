b4f1f176e4e653d11c7f1f5d6d8cadcd22fb8b85
pascal-vegt <9016712@student.zadkine.nl>
2024-02-14 11:43:14 +0100

    week 2 opdracht 2

---
+++ week2/opdracht 2/app.js
@@ -0,0 +1,11 @@
+let verlanglijstje = ["betere computer", "honderd euro", "een telefoon (geen apple! >:3", "geld om naar zweden te gaan :>", "spelletjes / cs2 skins :)"];
+
+for (let i = 0; i < verlanglijstje.length; i++) {
+  document.write(verlanglijstje[i] + "<br>");
+}
+
+//Maak een array genaamd "verlanglijst" en plaats daarin 5 items wat jij dit jaar voor kerst wilt hebben ;)
+
+//Toon met een for-loop alle items uit je array op het scherm onder elkaar. 
+
+
